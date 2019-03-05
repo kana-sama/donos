3f60c9004bd6b96c5476c540a36a586706029822
Andrei <andrew4chrome@gmail.com>
2019-03-05 15:33:46 +0300

    hotfix

--- lib/donos/telegram_api.ex
+++ lib/donos/telegram_api.ex
@@ -43,7 +43,7 @@ defmodule Donos.TelegramAPI do
           update.update_id + 1
         end)
       rescue
-        error -> offset
+        _ -> offset
       catch
         _ -> offset
       end
