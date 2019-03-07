869c21c18abf7853b48ac2d32f90773a7bfd234d
Andrei <andrew4chrome@gmail.com>
2019-03-07 14:34:36 +0300

    limit messages by one

--- lib/donos/bot/loop.ex
+++ lib/donos/bot/loop.ex
@@ -5,6 +5,8 @@ defmodule Donos.Bot.Loop do
   alias Nadia.Model.{Update, Message}
   alias Nadia.Model.{Audio, Document, Sticker, Video, Voice, Contact, Location}
 
+  @delay 100
+
   def start_link(_) do
     GenServer.start_link(__MODULE__, :none, name: __MODULE__)
   end
@@ -17,59 +19,24 @@ defmodule Donos.Bot.Loop do
 
   @impl GenServer
   def handle_info(:poll, offset) do
-    offset =
-      try do
-        {:ok, updates} = Nadia.get_updates(offset: offset)
+    case Nadia.get_updates(offset: offset, limit: 1, timeout: 100) do
+      {:ok, [update | _]} ->
+        try do
+          handle_update(update)
+          return(update.update_id + 1)
+        rescue
+          error ->
+            IO.inspect(error)
+            return(offset)
+        end
 
-        Enum.reduce(updates, offset, fn update, _ ->
-          case update do
-            %Update{message: %Message{text: <<"/", command::binary>>} = message} ->
-              Logic.post({:command, command}, message)
+      {:ok, []} ->
+        return(offset)
 
-            %Update{message: %Message{text: text} = message} when is_binary(text) ->
-              Logic.post({:text, text}, message)
-
-            %Update{message: %Message{audio: %Audio{} = audio} = message} ->
-              Logic.post({:audio, audio.file_id}, message)
-
-            %Update{message: %Message{document: %Document{} = document} = message} ->
-              Logic.post({:document, document.file_id}, message)
-
-            %Update{message: %Message{photo: [_ | _] = photos} = message} ->
-              Logic.post({:photo, Enum.at(photos, -1).file_id}, message)
-
-            %Update{message: %Message{sticker: %Sticker{} = sticker} = message} ->
-              Logic.post({:sticker, sticker.file_id}, message)
-
-            %Update{message: %Message{video: %Video{} = video} = message} ->
-              Logic.post({:video, video.file_id}, message)
-
-            %Update{message: %Message{voice: %Voice{} = voice} = message} ->
-              Logic.post({:voice, voice.file_id}, message)
-
-            %Update{message: %Message{contact: %Contact{} = contact} = message} ->
-              Logic.post({:contact, contact.phone_number, contact.first_name}, message)
-
-            %Update{message: %Message{location: %Location{} = location} = message} ->
-              Logic.post({:location, location.latitude, location.longitude}, message)
-
-            %Update{edited_message: %{text: text} = message} when is_binary(text) ->
-              Logic.edit({:text, text}, message)
-
-            _ ->
-              IO.inspect({:unkown_command, update})
-          end
-
-          update.update_id + 1
-        end)
-      rescue
-        error ->
-          IO.inspect({:logic_error, error})
-          offset
-      end
-
-    schedule_polling()
-    {:noreply, offset}
+      {:error, reason} ->
+        IO.inspect(reason)
+        return(offset)
+    end
   end
 
   @impl GenServer
@@ -77,7 +44,60 @@ defmodule Donos.Bot.Loop do
     {:stop, :normal, offset}
   end
 
+  defp handle_update(%Update{message: %Message{text: <<"/", command::binary>>} = message}) do
+    Logic.post({:command, command}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{text: text} = message}) when is_binary(text) do
+    Logic.post({:text, text}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{audio: %Audio{}} = message}) do
+    Logic.post({:audio, message.audio.file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{document: %Document{}} = message}) do
+    Logic.post({:document, message.document.file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{photo: [_ | _]} = message}) do
+    Logic.post({:photo, Enum.at(message.photos, -1).file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{sticker: %Sticker{}} = message}) do
+    Logic.post({:sticker, message.sticker.file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{video: %Video{}} = message}) do
+    Logic.post({:video, message.video.file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{voice: %Voice{}} = message}) do
+    Logic.post({:voice, message.voice.file_id}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{contact: %Contact{}} = message}) do
+    Logic.post({:contact, message.contact.phone_number, message.contact.first_name}, message)
+  end
+
+  defp handle_update(%Update{message: %Message{location: %Location{}} = message}) do
+    Logic.post({:location, message.location.latitude, message.location.longitude}, message)
+  end
+
+  defp handle_update(%Update{edited_message: %{text: text} = message}) when is_binary(text) do
+    Logic.edit({:text, text}, message)
+  end
+
+  defp handle_update(update) do
+    IO.inspect({:unkown_command, update})
+  end
+
   defp schedule_polling() do
-    Process.send_after(self(), :poll, 300)
+    Process.send_after(self(), :poll, @delay)
+  end
+
+  defp return(offset) do
+    schedule_polling()
+    {:noreply, offset}
   end
 end
