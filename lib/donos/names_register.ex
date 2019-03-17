cded0c8c9e7f32ba55dccba2637cea495c395681
Andrei <andrew4chrome@gmail.com>
2019-03-17 19:19:10 +0200

    use Agents for state-only servers

--- lib/donos/names_register.ex
+++ lib/donos/names_register.ex
@@ -1,24 +1,19 @@
 defmodule Donos.NamesRegister do
-  use GenServer
+  use Agent
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, :none, name: __MODULE__)
+    Agent.start_link(&init/0, name: __MODULE__)
   end
 
-  def new_name() do
-    GenServer.call(__MODULE__, :new_name)
+  def init do
+    Path.join(:code.priv_dir(:donos), "names.txt")
+    |> File.read!()
+    |> String.split("\n")
   end
 
-  @impl GenServer
-  def init(:none) do
-    names_resource = Path.join(:code.priv_dir(:donos), "names.txt")
-    names = File.read!(names_resource) |> String.split("\n")
-    {:ok, names}
-  end
-
-  @impl GenServer
-  def handle_call(:new_name, _, names) do
-    name = Enum.random(names)
-    {:reply, name, names}
+  def new_name do
+    Agent.get(__MODULE__, fn names ->
+      Enum.random(names)
+    end)
   end
 end
--- lib/donos/sessions_register.ex
+++ lib/donos/sessions_register.ex
@@ -1,39 +1,25 @@
 defmodule Donos.SessionsRegister do
-  use GenServer
+  use Agent
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, :none, name: __MODULE__)
+    Agent.start_link(fn -> Map.new() end, name: __MODULE__)
   end
 
   def get(user_id) do
-    GenServer.call(__MODULE__, {:get, user_id})
+    Agent.get(__MODULE__, fn sessions ->
+      Map.fetch(sessions, user_id)
+    end)
   end
 
   def register(user_id, session) do
-    GenServer.cast(__MODULE__, {:register, user_id, session})
+    Agent.update(__MODULE__, fn sessions ->
+      Map.put(sessions, user_id, session)
+    end)
   end
 
   def unregister(user_id) do
-    GenServer.cast(__MODULE__, {:unregister, user_id})
-  end
-
-  @impl GenServer
-  def init(:none) do
-    {:ok, Map.new()}
-  end
-
-  @impl GenServer
-  def handle_call({:get, user_id}, _, sessions) do
-    {:reply, Map.fetch(sessions, user_id), sessions}
-  end
-
-  @impl GenServer
-  def handle_cast({:register, user_id, session}, sessions) do
-    {:noreply, Map.put(sessions, user_id, session)}
-  end
-
-  @impl GenServer
-  def handle_cast({:unregister, user_id}, sessions) do
-    {:noreply, Map.delete(sessions, user_id)}
+    Agent.update(__MODULE__, fn sessions ->
+      Map.delete(sessions, user_id)
+    end)
   end
 end
