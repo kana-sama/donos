2b2fe9e757844122e25a4fb526691e29f86a1f78
kana-sama <andrew4chrome@gmail.com>
2020-01-06 05:59:46 +0300

    read blacklist config from env

--- config/config.prod.exs
+++ config/config.prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :donos,
   session_lifetime: 1000 * 60 * 30,
   history_channel: System.get_env("DONOS_HISTORY"),
-  blacklist: MapSet.new([])
+  blacklist: System.get_env("DONOS_BLACKLIST", "") |> String.split(",") |> MapSet.new()
 
 config :nadia,
   token: System.get_env("DONOS_TOKEN")
